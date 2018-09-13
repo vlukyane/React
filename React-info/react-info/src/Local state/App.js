441d810148ec595b467c47eedc7fe1f2e2800b97
Vyacheslav Lukyanenko <vlukyane@yandex.ru>
2018-09-13 12:57:15 +0300

    add local app.js for each example

---
+++ React-info/react-info/src/Local state/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Dropdown from "./Dropdown";
+
+class App extends Component {
+
+    render () {
+
+        return (
+            <div>
+                Hello react
+                <Dropdown/>
+            </div>
+        )
+    }
+
+}
+
+export default App;
