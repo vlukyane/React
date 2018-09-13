bc11736a99d4fffebe8a79a16796f05b30370d71
Vyacheslav Lukyanenko <vlukyane@yandex.ru>
2018-09-13 12:58:04 +0300

    this.props example

---
+++ React-info/react-info/src/Props/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Header from "./header";
+
+const menu = [
+    {
+        link: '/articles',
+        label: 'Articles'
+    },
+    {
+        link: '/contacts',
+        label: 'Contacts'
+    },
+    {
+        link: '/posts',
+        label: 'Posts'
+    }
+];
+
+class App extends Component {
+
+    render () {
+
+        return (
+            <div>
+                <Header items = {menu} />
+            </div>
+        )
+    }
+
+}
+
+export default App;
